ffad8864f87f4e78724252441e6d29be0aac7887
anacsv <anapaula.carneiro@olist.com>
2020-06-30 14:45:25 -0300

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,5 +1,11 @@
-A = [1,1]
-A.remove(1)
-A.remove(1)  
-A.remove(2)
-print(A)
+
+def solution(A):
+
+    for item in A:
+        if anterior == item:
+            A.remove
+        else:       
+        no_pair = anterior
+    return A 
+
+print(solution[9,3,9,3,9,7,9])    
